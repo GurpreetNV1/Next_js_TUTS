28b104f545f0473fff9518d2bf26ef1588d99e84
Gurpreet-Singh-01 <gurpreet.singh.work.29@gmail.com>
2026-03-30 19:22:52 +0530

    Basic Routing completed, Next Nested Routing

---
+++ 02_routing/src/app/about/page.js
@@ -0,0 +1,9 @@
+
+
+const page = () => {
+  return (
+    <h1>Holly fucking airball</h1>
+  )
+}
+
+export default page
---
+++ 02_routing/src/app/services/page.js
@@ -0,0 +1,9 @@
+
+
+const page = () => {
+  return (
+    <h1>Holly fucking Service</h1>
+  )
+}
+
+export default page
